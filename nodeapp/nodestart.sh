5d7c38f619b6df4fdc9cdfd19dfb062bdb0b277e
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-07-01 21:30:52 +0545

    Changed script

--- nodeapp/nodestart.sh
+++ nodeapp/nodestart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo su ec2-user
 cd /home/ec2-user/simple_todo || { echo "Could not find directory" exit 1; }
 #Install node app dependencies
 npm install || { echo "npm install failed"; exit 1; }
