d5c9582d284da125d0227cc22aad9ff123b9df6b
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-07-01 21:21:23 +0545

    Added debugging codes in script

--- nodeapp/nodestart.sh
+++ nodeapp/nodestart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /home/ec2-user/simple_todo
+cd /home/ec2-user/simple_todo || { echo "Could not find directory" exit 1; }
 #Install node app dependencies
-npm install
+npm install || { echo "npm install failed"; exit 1; }
 
 #Start the node app
-pm2 start server.js
+pm2 start server.js || { echo "node app could not start" exit 1;}
