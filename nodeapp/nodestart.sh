f9ad77cb072eb822700341e9c7b3cda0cd4b2e51
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-07-01 21:52:17 +0545

    Using node instead of pm2

--- nodeapp/nodestart.sh
+++ nodeapp/nodestart.sh
@@ -5,5 +5,5 @@ cd /home/ec2-user/simple_todo || { echo "Could not find directory" exit 1; }
 npm install || { echo "npm install failed"; exit 1; }
 
 #Start the node app
-cd /home/ec2-user/simple_todo
-pm2 start server.js || { echo "node app could not start" exit 1;}
+# pm2 start server.js || { echo "node app could not start" exit 1;}
+node server.js
