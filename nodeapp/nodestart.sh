b48587498e524fca9785429abff106296950a5f4
Sandesh Lama <sandesh@Sandeshs-MacBook-Pro.local>
2024-07-01 21:45:22 +0545

    Checking pm2 start

--- nodeapp/nodestart.sh
+++ nodeapp/nodestart.sh
@@ -5,4 +5,4 @@ cd /home/ec2-user/simple_todo || { echo "Could not find directory" exit 1; }
 npm install || { echo "npm install failed"; exit 1; }
 
 #Start the node app
-pm2 start server.js || { echo "node app could not start" exit 1;}
+pm2 start /home/ec2-user/simple_todo/server.js || { echo "node app could not start" exit 1;}
