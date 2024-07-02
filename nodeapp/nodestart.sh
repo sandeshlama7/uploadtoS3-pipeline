8adca3bacc831dcd048884d336834507c8093b61
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-07-02 11:07:56 +0545

    Testing

--- nodeapp/nodestart.sh
+++ nodeapp/nodestart.sh
@@ -4,5 +4,6 @@ cd /home/ec2-user/simple_todo || { echo "Could not find directory" exit 1; }
 #Install node app dependencies
 npm install || { echo "npm install failed"; exit 1; }
 
+# sleep 10
 #Start the node app
-pm2 start server.js || { echo "node app could not start" exit 1;}
+pm2 start server.js
