4ff1b4a4aaf27544f0cf9107c72f141d715ed99c
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-07-08 13:04:05 +0545

    Used environment varibales in Workflow, using files in ansible role

--- nodeapp/nodestart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ec2-user/simple_todo || { echo "Could not find directory" exit 1; }
-#Install node app dependencies
-npm install || { echo "npm install failed"; exit 1; }
-
-# sleep 10
-#Start the node app
-pm2 start server.js
